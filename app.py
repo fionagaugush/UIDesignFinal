0e3969dacd47041a11d279af6a85f8924e7aec77
Jyothi Gandi <jyothig.me@gmail.com>
2024-04-13 14:08:16 -0400

    add APIs for home, learn and quiz, along with tracking user current step

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from data import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+def update_user(new_data):
+    if 'step' not in new_data:
+        return False, "'step' is required"
+
+    data['user']['step'] = new_data['step']
+    data['user']['currentQuizQuestion'] = new_data.get('currentQuizQuestion', data['user']['currentQuizQuestion'])
+    data['user']['quizScore'] = new_data.get('quizScore', data['user']['quizScore'])
+
+    return True, None
+
+
+@app.route('/update_user_step', methods=['POST'])
+def update_user_step():
+    new_data = request.get_json()
+    success, error = update_user(new_data)
+    if not success:
+        return jsonify({'success': False, 'error': error}), 400
+    return jsonify({'success': True})
+
+
+@app.route('/learn')
+def learn():
+    update_user({'step': 1})
+    birds_data = data['birds']
+    return render_template('birdsList.html', birds=birds_data)
+
+
+@app.route('/quiz')
+def quiz():
+    quiz_data = data['quizQuestions']
+    return render_template('quiz.html', quiz=quiz_data)
 
 
 if __name__ == '__main__':
